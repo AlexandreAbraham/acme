10c66d0a06c2c9cf7c5c51b75d4b3c9f6b8775a1
Alexandre Abraham <abraham.alexandre@gmail.com>
2021-05-05 17:25:17 +0200

    Make menu better, or worse. Depends on the PoV

--- acme/refined_function.py
+++ acme/refined_function.py
@@ -1,6 +1,9 @@
 from copy import deepcopy
 
 
+def id_to_screen_name(id_name):
+    return ' '.join(s.capitalize() for s in id_name.split('_'))
+
 class InteractiveRefiner:
 
     def __init__(self, parameters):
@@ -9,12 +12,21 @@ class InteractiveRefiner:
         self.boxes = []
         for param in parameters:
             selected = wg.Checkbox(description=param.get("name"), value=True, indent=False)
-            type_ = wg.Dropdown(description='Type', options=['int', 'float', 'enum'])
-            # has_bounds = wg.Checkbox(description=param.get("name"), value=True, indent=False)
-            default = wg.Text(description='Default', value=str(param.get("default_value")))
-            hbox = wg.HBox([selected, type_, default])
-            self.parameters_widgets.append((selected, type_, default))
-            self.boxes.append(hbox)
+            selected.layout.width = '100%'
+            layout = {'width': 'auto'}
+            style = {'description_width': '120px'}
+            name = wg.Text(description='Screen name', value=id_to_screen_name(param.get("name")), layout=layout, style=style)
+            type_ = wg.Dropdown(description='Type', options=['Int', 'Doubles', 'Strings'], layout=layout, style=style)
+            default = wg.Text(description='Default', value=str(param.get("default_value")), layout=layout, style=style)
+            specs = wg.Text(description='Possible values', value='', layout=layout, style=style)
+            specs_details = wg.Label(value='Possible values can be [Lower, Upper] or {"A","B","C"} or range(min, max, step)', layout=layout, style=style)
+
+            box = wg.VBox([name, type_, default, specs, specs_details])
+            box.layout.margin = "0 0 0 50px"
+            box.layout.width = 'auto'
+            self.parameters_widgets.append([selected, name, type_, specs, default])
+            self.boxes.append(selected)
+            self.boxes.append(box)
     
     def display(self):
         from IPython.display import display
