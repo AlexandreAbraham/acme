af2dc2d6f76aef65047d7b4e6fa5599a4b97ec4a
Marsobad <marine.sobas@dataiku.com>
2021-05-06 13:32:37 +0200

    add plugin zip to dist

--- acme/plugin_generator.py
+++ acme/plugin_generator.py
@@ -7,13 +7,14 @@ from acme.plugin_parameter import IntPluginParameter, DoublesPluginParameter, St
 
 
 class PluginGenerator:
-    def __init__(self, import_name, prediction_type, refined_function, template_path='.', doc_url=""):
+    def __init__(self, import_name, prediction_type, refined_function, template_path='.', doc_url="", generate_plugin_zip=False):
         self.refined_module = refined_function
         self.import_name = import_name
         self.prediction_type = prediction_type
         self.doc_url = doc_url
         self.plugin_repository = f"dss-plugin-{self.refined_module.module_name}"
         self.template_repository = Path(template_path) / "templates"
+        self.generate_zip = generate_plugin_zip
 
     def write(self):
         self._write_plugin_json()
@@ -22,7 +23,8 @@ class PluginGenerator:
         self._write_algo_py(algorithm_name)
         self._write_python_lib()
         self._write_license()
-        shutil.make_archive(f"dss-plugin-{self.refined_module.module_name}", "zip", self.plugin_repository)
+        if self.generate_zip:
+            self._make_plugin()
 
     def _write_plugin_json(self):
         with open(f"{self.template_repository}/plugin_base/plugin.json") as plugin_json_file:
@@ -70,6 +72,10 @@ class PluginGenerator:
 
         with open(f"{self.plugin_repository}/LICENSE", "w") as outfile:
             outfile.write(util_script)
+
+    def _make_plugin(self):
+        Path(f"{self.plugin_repository}/dist").mkdir(parents=True, exist_ok=True)
+        shutil.make_archive(f"{self.plugin_repository}/dist/dss-plugin-{self.refined_module.module_name}", "zip", self.plugin_repository)
 
     def _format_parameter(self, new_parameter):
         parameter_type = new_parameter.get("type")
