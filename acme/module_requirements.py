e15ff36a7b44b16357cb1efbe9611bef1bf88836
Marsobad <marine.sobas@dataiku.com>
2021-05-06 14:33:55 +0200

    make get_requirements more realistic

--- acme/module_requirements.py
+++ acme/module_requirements.py
@@ -5,7 +5,16 @@ def get_requirements(module, package_name=None):
     if not package_name:
         module_name = module.__module__
         package_name = module_name.split(".")[0]
-    requirements_entry = subprocess.check_output(f"pipdeptree -p {package_name} --warn silence | grep -E '^\w+'", shell=True)
-    parsed_requirements = str(requirements_entry, "utf-8")
-    requirements_list = parsed_requirements.split("\n")[:-1]
+        print(f"Identified package name: {package_name}. If invalid, please specify the real package name to retrieve the requirements.")
+    try:
+        requirements_entry = subprocess.check_output(f"pipdeptree -p {package_name} --warn silence | grep -E '^\w+'", shell=True)
+    except subprocess.CalledProcessError:
+        requirements_entry = None
+        print("No requirement was retrieved")
+    if requirements_entry:
+        parsed_requirements = str(requirements_entry, "utf-8")
+        requirements_list = parsed_requirements.split("\n")[:-1]
+        print(f"Packages retrieved: {requirements_list}")
+    else:
+        requirements_list = []
     return requirements_list
