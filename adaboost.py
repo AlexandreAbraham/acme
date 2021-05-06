195622a85b49099b8ecb76f00198a50b40190d31
Alexandre Abraham <abraham.alexandre@gmail.com>
2021-05-06 08:20:56 +0200

    Rename refined_function to refiner

--- adaboost.py
+++ adaboost.py
@@ -1,5 +1,5 @@
 from acme.parser import parse_class
-from acme.refined_function import ModelRefiner
+from acme.refiner import ModelRefiner
 from acme.plugin_generator import PluginGenerator
 from sklearn.ensemble import AdaBoostClassifier
 
@@ -7,6 +7,6 @@ from sklearn.ensemble import AdaBoostClassifier
 import_name = "sklearn.ensemble"
 prediction_type = "BINARY_CLASSIFICATION"
 parsed_docstring = parse_class(AdaBoostClassifier)
-refined_function = ModelRefiner(parsed_docstring)
-plugin_generator = PluginGenerator("sklearn.ensemble", prediction_type, refined_function)
+refined_model = ModelRefiner(parsed_docstring)
+plugin_generator = PluginGenerator("sklearn.ensemble", prediction_type, refined_model)
 plugin_generator.write()
